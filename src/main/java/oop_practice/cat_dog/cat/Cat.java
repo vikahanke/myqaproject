5eba7c2fcaa80165ca9a7cc6ed0bc4278f7cde2d
viktoriiahanke <viktoriiahanke91@gmail.com>
2021-10-31 14:57:47 +0100

    Committ

--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -5,13 +5,19 @@ import oop_practice.cat_dog.dog.Dog;
 
 public class Cat extends Animal {
 
+    public static  String name;
+    public static  String animal;
+
     public Cat(String name) {
+        this.name = name;
 
     }
 
+
+
     @Override
     public void sound() {
-        System.out.println("I'am a Cat. My name Vasya. Mew");
+        System.out.println("I'am a" + animal + ". My name" + name + ". Mew");
     }
 
 }
--- src/main/java/oop_practice/cat_dog/dog/Dog.java
+++ src/main/java/oop_practice/cat_dog/dog/Dog.java
@@ -5,12 +5,17 @@ import oop_practice.cat_dog.animal.Animal;
 public class Dog extends Animal {
 
 
+    public static  String name;
+    public static  String animal;
+
     public Dog(String name) {
+        this.name = name;
 
     }
 
+
     @Override
     public void sound() {
-        System.out.println("I'am a Dog. My name is Tuzik. Gav");
+        System.out.println("I'am a" + animal + ". My name is" + name + ". Gav");
     }
 }
